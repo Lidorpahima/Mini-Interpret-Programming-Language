abab2c2407961fa2376a8fb7936e40ef2b54671c
Lidorpahima <lidorpahima28@gmail.com>
2024-07-27 20:12:15 +0300

    Push

--- Library/Tuples.py
+++ Library/Tuples.py
@@ -1,1 +1,13 @@
 #~~~~~Tuples~~~~~#
+def Tuples(*args):
+    return tuple(args)
+def sort(tupple):
+    return tuple(sorted(tupple))
+def addTuple(tuple1, tuple2):
+    return tuple(list(tuple1) + tuple2)
+def Getindex(tupple, i):
+    return tupple[i]
+def Index(tupple, i):
+    return tupple.index(i)
+def Length(tupple):
+    return len(tupple)
--- Project.py
+++ Project.py
@@ -1,19 +1,19 @@
-from Library.Arrays import *
-from Library.Loops import *
-from Library.Math import *
-from Library.String import *
+from Library import Arrays
+from Library import Loops
+from Library import Math
+from Library import String
     #~~~~~Main~~~~~~#
 def main():
-    new_array = New_Array()
-    Append(new_array,5)
-    Append(new_array, 52)
-    Append(new_array, 2)
-    Append(new_array, 4)
-    Length(new_array)
-    print(add(new_array,1,8))
+    new_array = Arrays.New_Array()
+    Arrays.Append(new_array,5)
+    Arrays.Append(new_array, 52)
+    Arrays.Append(new_array, 2)
+    Arrays.Append(new_array, 4)
+    Arrays.Length(new_array)
+    print(Arrays.add(new_array,1,8))
     print("CHECK")
     print(new_array)
-    print(Remove(new_array,-1))
+    print(Arrays.Remove(new_array,-1))
 
 if __name__ == '__main__':
     main()
